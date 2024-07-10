93d59372ef14a5c5a3d696237d72a34ee19cee40
Ignacio Fuentes <ignaciofuentes@outlook.com>
2024-07-10 12:20:32 -0400

    chat page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
+import { Amplify } from "aws-amplify";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+import outputs from "./amplify_outputs.json";
+import TodoList from './src/TodoList';
 
-export default function App() {
+Amplify.configure(outputs);
+
+const SignOutButton = () => {
+  const { signOut } = useAuthenticator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.signOutButton}>
+      <Button title="Sign Out" onPress={signOut} />
     </View>
   );
-}
+};
+
+
+const App = () => {
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        <SafeAreaView style={styles.container}>
+          <SignOutButton />
+          <TodoList />
+        </SafeAreaView>
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8,
+  },
+  signOutButton: {
+    alignSelf: "flex-end",
   },
 });
+export default App;
