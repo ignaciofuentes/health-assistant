e9354c2e1e54b28e0644e8c2bcba7e6ed3e1cc31
Ignacio Fuentes <ignaciofuentes@outlook.com>
2024-07-22 13:25:04 -0400

    adding files

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -7,7 +7,7 @@ import { storage } from "./storage/resource";
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
-  //data,
+  data,
   storage,
   auth,
 });
